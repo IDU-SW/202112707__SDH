d756df0a604cddf7591a2091b9b3ed9a42e97194
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-12-18 19:53:53 +0900

    Update App.js
    테스트 코드

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function TodoList({todos}) {
+    return (
+        <ul>
+        {todos.map( item => (
+            <li key={item.id}>
+                <input type="checkbox" checked={item.done}/> {item.text}
+            </li>
+        ))
+        }
+        </ul>
+    )
+}
+
+function TodoHeader({todos}) {
+    return (        
+        <h3>Todo App ( {todos.filter( item => item.done ).length } / {todos.length})</h3>        
+    )
+}
+
+function NewTodoForm({addTodo}) {
+    let [todo, setTodo] = useState('');
+    return (
+        <div>
+            <input placeholder='할일을 입력하세요.'/>
+            <button onClick={addTodo}>추가</button>
+        </div>
+    )
+}
+
+function App() {    
+    let [todos, setTodos] = useState([
+            {id: 1, text: 'study react.js', done: false},
+            {id: 2, text: 'study ReactNative', done: false},
+            {id: 3, text: 'study node.js', done: true}
+    ]);
+
+    function handleTodoAdd(todo) {
+        console.log('addTodo works :', todo);
+    }
+
+    return (
+        <div>
+            <TodoHeader todos={todos} />
+            <TodoList todos={todos} />
+            <NewTodoForm addTodo={handleTodoAdd}/>
+        </div>
+    );
 }
 
 export default App;
